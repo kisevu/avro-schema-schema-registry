f957a4e11a2e101638b1b3d66d14c39e976278d0
ameda-kevin <amedakevin@gmail.com>
2024-04-23 07:58:15 +0300

    boostrapped the project and added the require dependencies for working

---
+++ src/main/java/com/ameda/kisevu/apacheschemaregistry/ApacheSchemaRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.ameda.kisevu.apacheschemaregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheSchemaRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApacheSchemaRegistryApplication.class, args);
+    }
+
+}
